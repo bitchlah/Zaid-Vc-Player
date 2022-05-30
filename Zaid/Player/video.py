d28f1c7243f5a9fa76c3be55d1cd07dcee119cc7
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-05-30 20:56:02 +0700

    Update video.py

--- Zaid/Player/video.py
+++ Zaid/Player/video.py
@@ -2,13 +2,13 @@ import re
 import asyncio
 
 from config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2, IMG_6
-from Zaid.inline import stream_markup
+from ALBY.inline import stream_markup
 from Process.design.thumbnail import thumb
 from Process.design.chatname import CHAT_TITLE
-from Zaid.filters import command, other_filters
-from Zaid.queues import QUEUE, add_to_queue
-from Zaid.main import call_py, Test as user
-from Zaid.main import bot as Client
+from ALBY.filters import command, other_filters
+from ALBY.queues import QUEUE, add_to_queue
+from ALBY.main import call_py, Test as user
+from ALBY.main import bot as Client
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from pytgcalls import StreamType
